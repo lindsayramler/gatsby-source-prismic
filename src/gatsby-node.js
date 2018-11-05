4f218dff84a749f96c6ec940fe01b08fdfd13b4f
lbull144 <lindsay@pathwright.com>
2018-11-05 13:51:44 -0500

    initial commit

---
+++ src/gatsby-node.js
@@ -0,0 +1,29 @@
+import fetchData from './fetch'
+import { DocumentNode } from './nodes'
+
+exports.sourceNodes = async (
+  { boundActionCreators, getNodes, hasNodeChanged, store },
+  { repositoryName, accessToken }
+) => {
+  const {
+    createNode,
+    deleteNodes,
+    touchNode,
+    setPluginStatus,
+  } = boundActionCreators
+
+  const { documents } = await fetchData({
+    repositoryName,
+    accessToken,
+  })
+
+  documents.forEach(doc => {
+    const node = DocumentNode({
+      documentItem: doc,
+    })
+
+    createNode(node)
+  })
+
+  return
+}
